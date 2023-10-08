69cf1a3ff97a59857e17654b3cbe196d92e93ba5
wankhede04 <wankhedevijay12@gmail.com>
2023-10-08 20:37:44 +0530

    add network in hardhat config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,17 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import { config as dotEnvConfig } from "dotenv";
+
+dotEnvConfig();
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.18",
+  solidity: "0.8.19",
+  networks: {
+    swisstronik: {
+      url: process.env.SWISSTRON_RPC_URL,
+      accounts: [`0x${process.env.PRIVATE_KEY}`],
+    },
+  },
 };
 
 export default config;
